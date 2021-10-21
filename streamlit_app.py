cec8c9a9075f449fa07295f911cc1c2fc9c0da62
Carnisthor <j1n.l21k1s.e3k5l@gmail.com>
2021-10-21 21:42:55 +0200

    Tested edit function

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# TE - Smart City Challenge
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
