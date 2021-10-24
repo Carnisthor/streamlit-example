017abeac4af7ed15ccfee8d4cfe6f7d9e3356532
Carnisthor <j1n.l21k1s.e3k5l@gmail.com>
2021-10-24 18:10:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,7 @@ import pandas as pd
 import streamlit as st
 
 """
-# TE - Smart City Challenge
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+This is our entry for the AI and Data Products Challenge @Lara and @Clemens.
 """
 
 
