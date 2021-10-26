4cc626d793f0d655d30883e28d8c9c3532b6aae3
Carnisthor <j1n.l21k1s.e3k5l@gmail.com>
2021-10-26 21:17:23 +0200

    Testing out further stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,13 @@ This is our entry for the AI and Data Products Challenge @Lara and @Clemens.
 
 Insert some awesome introduction right here
 """
+
+chart_data = pd.DataFrame(
+  np.random.randn(20, 3),
+  columns=['a', 'b', 'c']
+)
+
+st.line_chart(chart_data)
+
 df = pd.read_csv("floor1_compressed.csv")
 st.line_chart(df[['date', 'sum']])
