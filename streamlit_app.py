7b15cdaa7e25bf898b1e21161e477f42c3289577
Carnisthor <j1n.l21k1s.e3k5l@gmail.com>
2021-10-26 21:57:18 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ This is our entry for the AI and Data Products Challenge @Lara and @Clemens.
 Insert some awesome introduction right here
 """
 
-chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
+chart_data = pd.DataFrame(np.random.randn(125, 3),columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data)
 
