e2da8f0886daf18bd3bcd658b77d50aabfa48703
Carnisthor <j1n.l21k1s.e3k5l@gmail.com>
2021-10-28 19:12:51 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,18 @@ col6.metric('Monetary', str("{:.2f}".format(potential_savings)) + ' EUR')
 st.caption('/* All energy savings are calculated on the basis that the AC is completly turned off during weekends.')
 
 st.header('Magic glass ball')
+# AR example
+from statsmodels.tsa.ar_model import AutoReg
+from random import random
+# contrived dataset
+data = [x + random() for x in range(1, 100)]
+# fit model
+model = AutoReg(data, lags=1)
+model_fit = model.fit()
+# make prediction
+yhat = model_fit.predict(len(data), len(data))
 
+st.info(yhat)
 
 st.header('Energy inspector')
 """
