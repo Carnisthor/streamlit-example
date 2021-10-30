0c582d5d356f63621f381dc641eabd0ba310d31d
Carnisthor <j1n.l21k1s.e3k5l@gmail.com>
2021-10-30 10:49:02 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,7 +77,7 @@ The energy inspector shows detailed information on what could be improved to sav
 d = st.date_input(label='Select a date to inspect', value=datetime.date(2019, 10, 28), min_value=datetime.date(2018, 7, 1), max_value=datetime.date(2019, 12, 31))
 #st.write(d)
 if d:
-  test = d.today().weekday()
+  test = d.weekday()
   st.write(test)
 else:
   a = 1
